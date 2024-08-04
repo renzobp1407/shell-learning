c4ff33ddee4ed54ae38e1f33883668956c2ccd7d
renzobp14 <rebarrio@w-tech-020.local>
2024-08-03 23:02:52 -0500

    first exercise

---
+++ hello_world.sh
@@ -0,0 +1,11 @@
+# numeral is for commentary and this is the initial line on the bash file, meand sha-bang
+#!/bin/bash
+
+# Command to show tipe of shell, in this case zsh
+ps | grep $$
+
+#type of bash 
+which bash
+
+#print hello world
+echo 'Hello, World'

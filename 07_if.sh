f347ea01e4633ea1dbbff113d89ed6f27383b091
Renzo Barrios <renzo.barrios1407@gmail.com>
2024-08-11 18:37:08 -0500

    [update] added rest of if practice and for loops begin

--- 07_if.sh
+++ 07_if.sh
@@ -24,3 +24,57 @@ elif [ "$NAME" = "George" ]; then
 else
   echo "This leaves us with Paul and Ringo"
 fi
+
+# Comparisons:
+
+# comparison    Evaluated to true when
+# $a -lt $b    $a < $b
+# $a -gt $b    $a > $b
+# $a -le $b    $a <= $b
+# $a -ge $b    $a >= $b
+# $a -eq $b    $a is equal to $b
+# $a -ne $b    $a is not equal to $b
+
+# string comparisons
+
+#comparison    Evaluated to true when
+#"$a" = "$b"     $a is the same as $b
+#"$a" == "$b"    $a is the same as $b
+#"$a" != "$b"    $a is different from $b
+#-z "$a"         $a is empty
+
+
+if [[ $VAR_A[0] -eq 1 && ($VAR_B = "bee" || $VAR_T = "tee") ]] ; then
+    echo "True"
+fi
+
+# Switch case structure
+
+mycase=1
+case $mycase in
+    1) echo "You selected bash";;
+    2) echo "You selected perl";;
+    3) echo "You selected phyton";;
+    4) echo "You selected c++";;
+    5) exit
+esac
+
+#!/bin/bash
+# change these variables
+NUMBER=16
+APPLES=16
+KING="LUIS"
+# modify above variables
+# to make all decisions below TRUE
+if [ $NUMBER -gt 15 ] ; then
+  echo 1
+fi
+if [ $NUMBER -eq $APPLES ] ; then
+  echo 2
+fi
+if [[ ($APPLES -eq 12) || ("$KING" = "LUIS") ]] ; then
+  echo 3
+fi
+if [[ $(($NUMBER + $APPLES)) -le 32 ]] ; then
+  echo 4
+fi
---
+++ 08_for_loops.sh
@@ -0,0 +1,17 @@
+# basic construct
+for arg in [list]
+do
+ command(s)...
+done
+
+# loop on array member
+NAMES=(Joe Jenny Sara Tony)
+for N in ${NAMES[@]} ; do
+  echo "My name is $N"
+done
+
+# loop on command output results
+for f in $( ls prog.sh /etc/localtime ) ; do
+  echo "File is: $f"
+done
+

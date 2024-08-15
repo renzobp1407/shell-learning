8ece66f81c8cd35119ddfe9333156e7810a90bdd
Renzo Barrios <renzo.barrios1407@gmail.com>
2024-08-14 23:14:09 -0500

    [update] added more loop examples

--- 08_for_loops.sh
+++ 08_for_loops.sh
@@ -42,3 +42,14 @@ until [ $COUNT -gt 5 ]; do
   COUNT=$(($COUNT + 1))
 done
 
+# break and continue
+# Prints out 0,1,2,3,4
+
+COUNT=0
+while [ $COUNT -ge 0 ]; do
+  echo "Value of COUNT is: $COUNT"
+  COUNT=$((COUNT+1))
+  if [ $COUNT -ge 5 ] ; then
+    break
+  fi
+done

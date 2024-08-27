09b088748794947d16abd80af19b119bb4c3dcd1
Renzo Barrios <renzo.barrios1407@gmail.com>
2024-08-26 22:48:39 -0500

    [update] added module 16 process substitution with more examples

--- 15_pipes.sh
+++ 15_pipes.sh
@@ -13,3 +13,25 @@ ls / | head
 ls / | grep  # This will grab any line/file that has a matching pattern in it
 
 cat /proc/cpuinfo | grep processor | wc -l
+
+# otro ejemplo mas claro:
+
+cat data.txt | grep "error" | wc -l
+
+# Cat data.txt muestra el contenido del  archivo
+# grep "error" filtra las lineas que contienen la palabra error
+# wx -l cuenta el numero de lineas que contienen la palabra error
+
+ls -l | grep "^-" | awk '{print $5, $9}' | sort -n -r
+
+# ls -l lista archivos del directorio actual con detalles
+# grep "^-"  filtra las lineas que comienzan con - que son archivos y no directorios
+# awk {print $5, $9} imprime el tamaño del archivo (quinta columnna) yu el nombre del archivo (novena columna)
+# sort -n -r: Ordena los archivos por tamaño numérico (-n) en orden descendente (-r).
+
+# imprimir la segunda columna con AWK
+Alice 25
+Bob 30
+Charlie 22
+
+awk '{print $2}' data.txt
--- 16_process_substitution.sh
+++ 16_process_substitution.sh
@@ -6,3 +6,21 @@ sort file1 > sorted_file1
 sort file2 > sorted_file2
 diff sorted_file1 sorted_file2
 
+# With process substitution you can do it in one line:
+
+
+diff <(sort file1) <(sort file2)
+
+# Imagine you want to store logs of an application into a file and at the same time print it on the console.
+# A very handy command for that is tee.
+
+# tee is for store logs ans print to the console
+
+echo "hello, world!" | tee /tmp/hello.txt
+
+# Now let say you want to have only lower case characters in the file but keep the regular case on the output.
+# You could use process substitution that way:
+
+echo "Hello, world!" | tee >(tr '[:upper:]' '[:lower:]' > /tmp/hello.txt)
+
+
